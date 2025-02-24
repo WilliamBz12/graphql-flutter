5e01cbd4f06d143a966b0af80e16945e122b2b4a
Williambz12 <williamsilva754@gmail.com>
2025-02-24 18:11:51 -0300

    video 1.4

---
+++ lib/data/repositories/remote_task_repository.dart
@@ -0,0 +1,52 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:sqlite_offline/data/repositories/task_repository.dart';
+import 'package:sqlite_offline/domain/models/task/task.dart';
+
+class RemoteTaskRepository implements TaskRepository {
+  final GraphQLClient client;
+  RemoteTaskRepository({required this.client});
+
+  @override
+  Future<int> addTask(Task task) {
+    // TODO: implement addTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> deleteTask(int id) {
+    // TODO: implement deleteTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Task>> getTasks() async {
+    const queryTasks = '''
+query {
+  tasks {
+    id
+    title
+    isCompleted
+    description
+    category
+    created_at
+  }
+}
+''';
+
+    final result = await client.query(
+      QueryOptions(document: gql(queryTasks)),
+    );
+
+    if (result.hasException) {
+      throw Exception();
+    }
+
+    final data = result.data?['data']['tasks'] as List?;
+  }
+
+  @override
+  Future<bool> updateTask(Task task) {
+    // TODO: implement updateTask
+    throw UnimplementedError();
+  }
+}
