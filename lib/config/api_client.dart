7d4f28af9052f881675be9eca0523f30a6d44b51
Williambz12 <williamsilva754@gmail.com>
2025-02-24 18:02:43 -0300

    video 1.3

---
+++ lib/config/api_client.dart
@@ -0,0 +1,17 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+class ApiClient {
+  static GraphQLClient create() {
+    final httpLink =
+        HttpLink('https://uncommon-maggot-51.hasura.app/v1/graphql');
+    final authLink = AuthLink(
+      getToken: () => const String.fromEnvironment('HASURA_TOKEN'),
+      headerKey: 'x-hasura-admin-secret',
+    );
+
+    final link = authLink.concat(httpLink);
+    final cache = GraphQLCache();
+    final client = GraphQLClient(link: link, cache: cache);
+    return client;
+  }
+}
